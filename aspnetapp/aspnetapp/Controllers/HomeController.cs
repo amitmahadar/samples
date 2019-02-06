95f3879c8651f894c8fd9fd1c7a2e10c031ff29c
amitmahadar <46504805+amitmahadar@users.noreply.github.com>
2019-02-06 12:23:26 -0600

    test

--- aspnetapp/aspnetapp/Controllers/HomeController.cs
+++ aspnetapp/aspnetapp/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace aspnetapp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Claims Manager";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "Technical Support:";
 
             return View();
         }
